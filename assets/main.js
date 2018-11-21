4f287691cf948481a50755aa657cbad1bc7f676e
Procureline <subhash.viswakarma@procuretechstaff.in>
2018-11-21 13:30:56 +0530

    final code

--- assets/main.js
+++ assets/main.js
@@ -345,10 +345,10 @@ var AuthGuardService = /** @class */ (function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Config", function() { return Config; });
 var Config = {
-    //  api_endpoint: 'https://ptsexamportal.azurewebsites.net/api',
-    //  cv_endpoint: 'https://ptsexamportal.azurewebsites.net'
-    api_endpoint: 'http://localhost:1337/api',
-    cv_endpoint: 'http://localhost:1337',
+    api_endpoint: 'https://ptsexamportal.azurewebsites.net/api',
+    cv_endpoint: 'https://ptsexamportal.azurewebsites.net'
+    //  api_endpoint: 'http://localhost:1337/api',
+    //  cv_endpoint: 'http://localhost:1337',
 };
 
 
